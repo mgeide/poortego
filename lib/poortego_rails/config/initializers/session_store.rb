b8d08dfb0981582b5de423f931cd28155b068203
missile <mgeide@gmail.com>
2014-01-21 06:26:44 -0800

    Removed old files

--- lib/poortego_rails/config/initializers/session_store.rb
+++
@@ -1,8 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-PoortegoRails::Application.config.session_store :cookie_store, key: '_poortego-rails_session'
-
-# Use the database for sessions instead of the cookie-based default,
-# which shouldn't be used to store highly confidential information
-# (create the session table with "rails generate session_migration")
-# PoortegoRails::Application.config.session_store :active_record_store
--- transforms/domain_type/unit_tests/resolv_test.rb
+++
@@ -1,8 +0,0 @@
-
-require 'resolv'
-
-dns_resolver = Resolv::DNS.new()
-resources = dns_resolver.getresources("google.com", Resolv::DNS::Resource::IN::A)
-resources.each do |resource|
-  puts "#{resource.address}"
-end
